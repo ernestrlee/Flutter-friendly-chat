7050a8c5bbc95f79088f904a32b5e18a9c9b3549
ernestrlee <ernestrlee@yahoo.com>
2020-06-25 10:39:38 -0700

    Added text composer widget.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,30 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return  Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(
+            hintText: 'Send a message'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +49,7 @@ class ChatScreen extends StatelessWidget {
           'FriendlyChat'
         ),
       ),
+      body: _buildTextComposer(),
     );
   }
 }
