4bfc50f1b67393c8d12984b171d3fc136ce035af
ernestrlee <ernestrlee@yahoo.com>
2020-06-25 10:42:35 -0700

    Added send button.

--- lib/main.dart
+++ lib/main.dart
@@ -32,11 +32,23 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget _buildTextComposer() {
     return  Container(
       margin: EdgeInsets.symmetric(horizontal: 8.0),
-      child: TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: InputDecoration.collapsed(
-            hintText: 'Send a message'),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(
+                  hintText: 'Send a message'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)),
+          ),
+        ],
       ),
     );
   }
